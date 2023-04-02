8ba302dbb02946784a59ddf6b03455a50207739e
nztech01 <ian@computerdoctor.online>
2023-04-03 10:18:47 +1200

    linked css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import type { AppProps } from 'next/app'
+import "./App.css"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-  <h2>Create images with your mind</h2>
-  <textarea
-  className="app-input"
-  placeholder="Create any type of Image you can think of with as much added descrition as you would like"
-   />
-   <button>Generate Image</button>
-   </>
+  return <div className='app-main'>
+    <h2>Create images with your mind</h2>
+    <textarea
+      className="app-input"
+      placeholder="Create any type of Image you can think of with as much added descrition as you would like"
+    />
+    <button>Generate Image</button>
+  </div>
 
 }
