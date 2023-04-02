a542bd440a27279b388aaed189e206d2e0a34feb
nztech01 <ian@computerdoctor.online>
2023-04-03 09:28:47 +1200

    added text box

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,5 +3,10 @@ import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
   return <>
   <h2>Create images with your mind</h2>
+  <textarea
+  className="app-input"
+  placeholder="Create any type of Image you can think of with as much added descrition as you would like"
+   />
    </>
+
 }
