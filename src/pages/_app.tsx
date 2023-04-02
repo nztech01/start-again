21109e13558073f5cb94a8c4b343865644057794
nztech01 <ian@computerdoctor.online>
2023-04-03 09:35:17 +1200

    added button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   className="app-input"
   placeholder="Create any type of Image you can think of with as much added descrition as you would like"
    />
+   <button>Generate Image</button>
    </>
 
 }
