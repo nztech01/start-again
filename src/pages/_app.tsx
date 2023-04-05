650828b516206dfb62913b468e69c5dc7cb4f195
nztech01 <ian@computerdoctor.online>
2023-04-05 19:34:43 +1200

    api13

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import "./App.css";
 import { Configuration, OpenAIApi } from "openai";
-import getConfig from "next/config";
+import getConfig from 'next/config';
 
 export default function App({ Component, pageProps }: AppProps) {
 
@@ -13,23 +13,23 @@ export default function App({ Component, pageProps }: AppProps) {
   const configuration = new Configuration({ apiKey });
   const openai = new OpenAIApi(configuration);
 
-    const generateImage = async () => {
+  const generateImage = async () => {
 
-      const res = await openai.createImage({
-        prompt: "a white siamese cat",
-        n: 1,
-        size: "1024x1024"
-      })
-      const data = res.data;
-      console.log(data);
-    }
+    const res = await openai.createImage({
+      prompt: "a white siamese cat",
+      n: 1,
+      size: "1024x1024"
+    })
+    const data = res.data;
 
-    return <div className='app-main'>
-      <h2>Create images with your mind</h2>
-      <textarea
-        className="app-input"
-        placeholder="Create any type of Image you can think of with as much added descrition as you would like"
-      />
-      <button onClick={generateImage}>Generate Image</button>
-    </div>
-  };
+  }
+
+  return <div className='app-main'>
+    <h2>Create images with your mind</h2>
+    <textarea
+      className='app-input'
+      placeholder="Create any type of Image you can think of with as much added descrition as you would like"
+    />
+    <button onClick={generateImage}>Generate Image</button>
+  </div>
+};
